c4382094f39c44905ba6ee5b6d5f535b29f9f328
luis-borges <kdegrize@gmail.com>
2024-01-28 21:23:26 +0100

    change role

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
                   Hi, I'm <span className="text-[#915eff]">Kechiedou</span>
               </h1>
               <p className={`${styles.heroSubText} text-white-100`}>
-                  I develop 3D visuals, user <br className="sm:block hidden"/> interfaces and web applications
+                  I develop user <br className="sm:block hidden"/> interfaces, web and mobile applications
               </p>
           </div>
         </div>
